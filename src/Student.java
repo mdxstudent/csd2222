5b200f4d4a430a053e868f5f74834efe9984cefc
sp1499 <sp1499@PC-00191563.uni.mdx.ac.uk>
2014-11-04 10:12:50 +0000

    this is the work

---
+++ src/Student.java
@@ -0,0 +1,21 @@
+
+public class Student {
+	private String name;
+	private int age;
+	private char gender;
+	private String module;
+	
+	public Student (String name, int age, char gender, String module){
+		
+		this.name = name;
+		this.age = age;
+		this.gender = gender;
+		this.module = module;
+		
+	}
+	
+
+	public void printDetails ( ){
+		System.out.println("name: " + name + "age: " + age + "gender: " + gender + "module:" + module );
+	}
+}
